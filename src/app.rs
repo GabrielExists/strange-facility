03942e114cc83ad7d9c180e2cca544b15e4cfcba
Gabriel <gabriel.exists.concurrently@gmail.com>
2024-12-02 11:47:56 +0100

    Import version 2

--- src/app.rs
+++ src/app.rs
@@ -1,33 +1,41 @@
 // #![cfg(target_arch = "wasm32")]
 
+use std::collections::BTreeMap;
 use yew::prelude::*;
 use std::fmt::{Display, Formatter};
+use std::iter::{zip, Zip};
+use std::slice::Iter;
 use crate::jobs::*;
-// use serde::{Deserialize, Serialize};
-// use serde_wasm_bindgen::to_value;
-// use wasm_bindgen::prelude::*;
-// use wasm_bindgen_futures::spawn_local;
 
 pub struct App {
-    user_error: Option<String>,
-    applied_buttons: Vec<Job>,
+    pub user_error: Option<String>,
+    pub applied_jobs: Vec<Job>,
+    pub view_cache: ViewCache,
 }
 
 #[derive(Clone, Debug)]
 pub enum AppMessage {
-    AddJob (Job),
+    AddJob(Job),
     RemoveHistory(usize),
 }
 
+pub struct ViewCache {
+    pub job_and_ok: Vec<(Job, bool)>,
+    pub resource_lists: Vec<Vec<(Resource, i64)>>,
+    pub seen_resources: Vec<Resource>,
+}
 
 impl Component for App {
     type Message = AppMessage;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
+        let applied_jobs = vec![Job::starting_resources()];
+        let (view_cache, user_error) = App::create_view_cache(&applied_jobs);
         Self {
-            user_error: None,
-            applied_buttons: Vec::new()
+            user_error,
+            applied_jobs,
+            view_cache,
         }
     }
 
@@ -35,22 +43,15 @@ impl Component for App {
         self.user_error = None;
         match msg {
             AppMessage::AddJob(job) => {
-                self.applied_buttons.push(job);
-                // let new_resources = apply_job(self.resources.clone(), job);
-                // match new_resources {
-                //     Ok(new_resources) => {
-                //        self.resources = new_resources;
-                //     }
-                //     Err(error_message) => {
-                //         self.user_error = Some(error_message);
-                //     }
-                // }
+                self.applied_jobs.push(job);
+                self.refresh_view_cache();
                 true
             }
             AppMessage::RemoveHistory(index) => {
-                if index < self.applied_buttons.len() {
-                    self.applied_buttons.remove(index);
+                if index < self.applied_jobs.len() {
+                    self.applied_jobs.remove(index);
                 }
+                self.refresh_view_cache();
                 true
             }
         }
@@ -59,7 +60,18 @@ impl Component for App {
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
         <div class="flex flex-row">
-            <div class="w-4/5 h-screen border border-slate-800 bg-blue-100 flex-col">
+            <div class="h-screen border border-slate-800 bg-blue-100 flex-col">
+                <div class="p-4 te">
+                    {"This game is very WIP, but there's some stuff to play around with. Actions marked in red failed due to lack of resources. Try to successfully depart in the fewest days possible."}
+                </div>
+                // Current error
+                { if self.user_error.is_some() {
+                    html!{ <div class="flex flex-row gap-y-2 p-2 border border-red-600"> {&self.user_error} </div> }
+                } else {
+                    html!{ <div></div> }
+                }
+                }
+                // List available jobs
                 <div class="flex flex-row gap-y-2">
                     { for Job::list_jobs().into_iter().map(|job| {
                         let callback_job = job.clone();
@@ -69,46 +81,217 @@ impl Component for App {
                             </button>
                         }
                     })}
+                    // Total jobs
                     <div class="border border-slate-900 background-slate-100 p-2 ml-4">
-                        {format!("Total jobs ran: {}", self.applied_buttons.len())}
+                        {format!("Total days spent: {}", self.applied_jobs.len())}
                     </div>
                 </div>
-                <div class="flex flex-row gap-y-2">
-                    { for (apply_jobs_partial(starting_resources(), self.applied_buttons.clone())).iter().map(|resource| {
-                        html! {
-                            <div class="border border-slate-900 p-2">
-                                {format!("{}: {}", resource.0.to_string(), resource.1)}
-                            </div>
-                        }
-                    })}
 
-                </div>
-                <div class="flex flex-row gap-y-2">
-                    {&self.user_error}
-                </div>
-                <div class="flex flex-col gap-y-2">
-                    { for get_job_success_zip(starting_resources(), self.applied_buttons.clone()).into_iter().enumerate().map(|(index, (job, is_ok))| {
+                // History of jobs
+            // <div class="flex flex-col gap-y-2">
+            // { for self.view_cache.job_and_ok.iter().enumerate().map(|(index, (job, is_ok))| {
+            //     html! {
+            //         <button class={
+            //             "border border-slate-900 background-slate-100 p-2 flex flex-row items-center justify-center"
+            //         } onclick={ctx.link().callback(move |_event: MouseEvent| AppMessage::RemoveHistory(index))}>
+            //             <div class={if *is_ok {"bg-red-600 w-1 h-6 p-1 mr-2 collapse"} else {"bg-red-600 w-1 h-6 p-1 mr-2 visible"}} ></div>
+            //             {job.button_text}
+            //         </button>
+            //     }
+            // })}
+                // </div>
+                                // "border border-slate-900 background-slate-100 p-2"
+                // Table of resource steps and jobs
+                <table class="table-auto">
+                    <thead>
+                        <tr>
+                            <td class="border border-slate-900 background-slate-100 p-2">
+                                {"Task for today"}
+                            </td>
+                    { for self.view_cache.seen_resources.iter().map(|resource| {
                         html! {
-                            <button class={
-                                "border border-slate-900 background-slate-100 p-2 flex flex-row items-center justify-center"
-                            } onclick={ctx.link().callback(move |_event: MouseEvent| AppMessage::RemoveHistory(index))}>
-                                <div class={if is_ok {"bg-red-600 w-1 h-6 p-1 mr-2 collapse"} else {"bg-red-600 w-1 h-6 p-1 mr-2 visible"}} ></div>
-                                {job.button_text}
-                            </button>
+                            <td class="border border-slate-900 background-slate-100 p-2">
+                                {resource.to_string()}
+                            </td>
                         }
                     })}
-                </div>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    { for self.get_table_zip().enumerate().rev().map(|(index, ((job, is_ok), resources))| {
+                        html! {
+                        <tr>
+                            <td class="border border-slate-900 background-slate-100">
+                            { if job.button {
+                                html! {
+                                <button class={
+                                    "border border-slate-900 background-slate-100 p-2 flex flex-row items-center justify-center"
+                                } onclick={ctx.link().callback(move |_event: MouseEvent| AppMessage::RemoveHistory(index))}>
+                                    <div class={if *is_ok {"bg-red-600 w-1 h-6 p-1 mr-2 collapse"} else {"bg-red-600 w-1 h-6 p-1 mr-2 visible"}} ></div>
+                                    {job.button_text}
+                                </button>
+                                }
+                            } else {
+                                html! {
+                                <div class={ "background-slate-100 p-2 flex flex-row items-center justify-center" }>
+                                    <div class={if *is_ok {"bg-red-600 w-1 h-6 p-1 mr-2 collapse"} else {"bg-red-600 w-1 h-6 p-1 mr-2 visible"}} ></div>
+                                    {job.button_text}
+                                </div>
+                                }
+                            } }
+                            </td>
+                            { for resources.iter().map(|(_resource, amount)| {
+                                html! {
+                            <td class="border border-slate-900 background-slate-100 p-2">
+                                    <div class="flex">
+                                <div class={if *amount >= 0 {"collapse"} else {"bg-red-600 w-1 h-6 p-1 mr-2 visible"}} ></div>
+                                {amount.to_string()}
+                                    </div>
+                            </td>
+                                }
+                            })}
+                        </tr>
+                        }
+                    })}
+                    </tbody>
+                </table>
             </div>
-            <div class="w-1/5 h-screen border border-slate-800 bg-blue-300">
-            </div>
+            // <div class="w-1/5 h-screen border border-slate-800 bg-blue-300">
+            // </div>
         </div>
         }
     }
 }
 
+impl App {
+    pub fn refresh_view_cache(&mut self) {
+        let (view_cache, user_error) = Self::create_view_cache(&self.applied_jobs);
+        self.user_error = user_error;
+        self.view_cache = view_cache;
+    }
+
+    pub fn create_view_cache(applied_jobs: &Vec<Job>) -> (ViewCache, Option<String>) {
+        let mut job_and_ok = Vec::new();
+        let mut resource_sets = Vec::new();
+        let mut user_error = None;
+        let mut seen_resources = Vec::new();
+        let mut resources = BTreeMap::new();
+        for job in applied_jobs.iter() {
+            let (new_resources, result) = apply_job(resources.clone(), &job);
+            for resource in new_resources.keys() {
+                if !seen_resources.contains(resource) {
+                    seen_resources.push(resource.clone());
+                }
+            }
+            match result {
+                Ok(()) => {
+                    job_and_ok.push((job.clone(), true));
+                    resource_sets.push(new_resources.clone());
+                    resources = new_resources;
+                    user_error = None;
+                }
+                Err(error_message) => {
+                    job_and_ok.push((job.clone(), false));
+                    resource_sets.push(new_resources.clone());
+                    user_error = Some(error_message);
+                }
+            }
+        }
+        // seen_resources.append(&mut resources);
+//     for index in 0..jobs.len() {
+//         let previous_resources = apply_jobs_partial(resources.clone(), jobs[0..index].to_vec());
+//         let job = jobs.get(index).expect("This index should be valid.").clone();
+//         let success = apply_job(previous_resources, job.clone()).is_ok();
+//         results.push((job, success));
+//     }
+
+        Self::prune(&mut seen_resources);
+        let resource_lists = Self::normalize_list(&resource_sets, &seen_resources);
+
+        let view_cache = ViewCache {
+            job_and_ok,
+            resource_lists,
+            seen_resources,
+        };
+        (view_cache, user_error)
+    }
+
+    // fn norm(resource_sets: &mut Vec<BTreeMap<Resource, u64>>, resources: &mut Resources) {
+    //     let attributes = attributes();
+    //     for resource in resources.iter() {
+    //         let visible = attributes.get(resource.0).map(|atts| atts.visible).unwrap_or(true);
+    //         for set in resource_sets.iter_mut() {
+    //             if !visible {
+    //                 set.remove(resource.0);
+    //             } else {
+    //                 set.entry(resource.0.clone()).or_insert(0);
+    //             }
+    //         }
+    //     }
+    //     for (resource, attribute) in attributes.iter() {
+    //         if !attribute.visible {
+    //             resources.remove(resource);
+    //         }
+    //     }
+    // }
+    //
+
+    fn normalize_list(resource_sets: &Vec<ResourceSet>, seen_resources: &Vec<Resource>) -> Vec<Vec<(Resource, i64)>>{
+        resource_sets.iter().map(|set|{
+            Self::normalize(set, seen_resources)
+        }).collect()
+    }
+    fn normalize(resource_set: &ResourceSet, seen_resources: &Vec<Resource>) -> Vec<(Resource, i64)> {
+        let mut result = Vec::new();
+        for resource in seen_resources.iter() {
+            result.push((resource.clone(), resource_set.get(resource).map(|number| number.clone()).unwrap_or(0)));
+        }
+        result
+    }
+
+    fn prune(resources: &mut Vec<Resource>) {
+        let attributes = attributes();
+        resources.retain(|item|{
+            attributes.get(item).map(|attribute| attribute.visible).unwrap_or(true)
+        });
+    }
+
+    fn get_table_zip(&self) -> Zip<Iter<(Job, bool)>, Iter<ResourceList>> {
+        zip(self.view_cache.job_and_ok.iter(), self.view_cache.resource_lists.iter())
+    }
+    // fn get_starting_resources(&self) -> Resources {
+    //     let mut resources = starting_resources();
+    //     Self::prune_and_normalize(&mut resources, &self.view_cache.resources);
+    //     resources
+    // }
+    // fn get_visible_resources(&self) -> Resources {
+    //     let attributes = attributes();
+    //     apply_jobs_partial(starting_resources(), self.applied_jobs.clone())
+    //         .into_iter()
+    //         .filter(|(resource, _num)| {
+    //             attributes.get(resource).map(|att| att.visible).unwrap_or(true)
+    //         })
+    //         .collect()
+    // }
+    //
+    // fn get_visible_resources_first_n(&self, n: usize) -> Resources {
+    //     let attributes = attributes();
+    //     apply_jobs_partial(starting_resources(), self.applied_jobs.clone())
+    //         .into_iter()
+    //         .take(n)
+    //         .filter(|(resource, _num)| {
+    //             attributes.get(resource).map(|att| att.visible).unwrap_or(true)
+    //         })
+    //         .collect()
+    // }
+}
+
 impl Display for Resource {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
+            Resource::Player => {
+                f.write_str("Player")
+            }
             Resource::Scrap => {
                 f.write_str("Scrap")
             }
@@ -120,6 +303,12 @@ impl Display for Resource {
             }
             Resource::FoodRation => {
                 f.write_str("Food ration")
+            }
+            Resource::AirTank => {
+                f.write_str("Air tank")
+            }
+            Resource::AirTankPressurizer => {
+                f.write_str("Air tank pressurizer")
             }
         }
     }
--- src/jobs.rs
+++ src/jobs.rs
@@ -1,83 +1,166 @@
 #![allow(dead_code)]
+
 use std::collections::BTreeMap;
 
-pub type Resources = BTreeMap<Resource, u64>;
+
+#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub enum Amount {
+    Gain(i64),
+    Spend(i64),
+    Catalyst(i64),
+    GainX(i64),
+    SpendX(i64),
+    CatalystX(i64),
+}
+
+impl Amount {
+    pub fn multiply(self, factor: i64) -> Self {
+        match self {
+            Amount::Gain(delta) => Amount::Gain(delta * factor),
+            Amount::Spend(delta) => Amount::Spend(delta * factor),
+            Amount::Catalyst(delta) => Amount::Catalyst(delta * factor),
+            Amount::GainX(delta) => Amount::GainX(delta * factor),
+            Amount::SpendX(delta) => Amount::SpendX(delta * factor),
+            Amount::CatalystX(delta) => Amount::CatalystX(delta * factor),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub enum Resource {
+    Player,
+    Scrap,
+    SpareParts,
+    Fish,
+    FoodRation,
+    AirTank,
+    AirTankPressurizer,
+}
+
+pub struct ResourceAttributes {
+    pub upkeep: Vec<(Resource, Amount)>,
+    pub visible: bool,
+}
+
+pub type ResourceSet = BTreeMap<Resource, i64>;
+pub type ResourceList = Vec<(Resource, i64)>;
+pub type AttributeMappings = BTreeMap<Resource, ResourceAttributes>;
+
 #[derive(Clone, Debug)]
 pub struct Job {
     pub button_text: &'static str,
     pub delta: Vec<(Resource, Amount)>,
+    pub button: bool,
+}
+
+pub fn attributes() -> AttributeMappings {
+    BTreeMap::from([
+        (Resource::Player, ResourceAttributes {
+            upkeep: Vec::from([(Resource::FoodRation, Amount::Spend(1))]),
+            visible: false,
+        }),
+        (Resource::AirTankPressurizer, ResourceAttributes {
+            upkeep: Vec::from([(Resource::AirTank, Amount::Gain(1))]),
+            visible: true,
+        }),
+    ])
 }
 
 impl Job {
+    pub fn starting_resources() -> Job {
+        Job {
+            button_text: "Starting resources",
+            delta: vec![
+                (Resource::Player, Amount::Gain(1)),
+                (Resource::FoodRation, Amount::Gain(5)),
+            ],
+            button: false,
+        }
+    }
+
     pub fn list_jobs() -> Vec<Job> {
         vec![
             Job {
                 button_text: "Deconstruct derelict machines",
                 delta: vec![
                     (Resource::Scrap, Amount::Gain(3)),
-                    (Resource::FoodRation, Amount::Spend(1)),
                 ],
+                button: true,
             },
             Job {
                 button_text: "Toil in the forge",
                 delta: vec![
                     (Resource::Scrap, Amount::Spend(5)),
                     (Resource::SpareParts, Amount::Gain(1)),
-                    (Resource::FoodRation, Amount::Spend(1)),
                 ],
+                button: true,
             },
             Job {
                 button_text: "Collect the ocean's bounty, by submarine and net",
                 delta: vec![
                     (Resource::Fish, Amount::Gain(10)),
-                    (Resource::FoodRation, Amount::Spend(1)),
                 ],
+                button: true,
             },
             Job {
                 button_text: "Operate Martha, the canning machine",
                 delta: vec![
-                    (Resource::FoodRation, Amount::Spend(1)),
                     (Resource::Fish, Amount::SpendX(10)),
                     (Resource::FoodRation, Amount::GainX(3)),
                 ],
+                button: true,
+            },
+            Job {
+                button_text: "Jury rig an air pressurizer",
+                delta: vec![
+                    (Resource::SpareParts, Amount::Spend(2)),
+                    (Resource::AirTankPressurizer, Amount::Gain(1)),
+                ],
+                button: true,
             },
             Job {
                 button_text: "Make submarine worthy of the deep seas and depart (winning the game)",
                 delta: vec![
                     (Resource::SpareParts, Amount::Spend(10)),
-                    (Resource::FoodRation, Amount::Spend(1)),
+                    (Resource::AirTank, Amount::Spend(60)),
                 ],
+                button: true,
             },
         ]
     }
-}
-pub fn starting_resources() -> Resources {
-    BTreeMap::from([
-        (Resource::FoodRation, 5)
-    ])
-}
-
-#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub enum Resource {
-    Scrap,
-    SpareParts,
-    Fish,
-    FoodRation,
+// pub fn list_visible_jobs() -> Vec<Job> {
+//     let attributes = attributes()
+//     Self::list_jobs().into_iter().map(|item| {
+//         attributes.get()
+//     })
+// }
 }
 
-#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub enum Amount {
-    Gain(u64),
-    Spend(u64),
-    Catalyst(u64),
-    GainX(u64),
-    SpendX(u64),
-    CatalystX(u64),
+pub fn apply_job(orig_resources: ResourceSet, job: &Job) -> (ResourceSet, Result<(), String>) {
+    let mut resources = orig_resources.clone();
+    for (current_resource, attribute) in attributes() {
+        if let Some(num) = resources.get(&current_resource) {
+            let delta = attribute.upkeep
+                .into_iter()
+                .map(|(delta_resource, delta_amount)| {
+                    (delta_resource, delta_amount.multiply((*num).clone()))
+                })
+                .collect();
+
+            let (new_resources, result) = apply_delta(resources, &delta);
+            if let Err(error) = result {
+                return (new_resources, Err(error));
+            }
+            resources = new_resources;
+        }
+    }
+    apply_delta(resources, &job.delta)
 }
 
-pub fn apply_job(mut resources: Resources, job: Job) -> Result<Resources, String> {
+pub fn apply_delta(mut resources: ResourceSet, delta: &Vec<(Resource, Amount)>) -> (ResourceSet, Result<(), String>) {
     let mut x = None;
-    for (resource, amount) in job.delta.iter() {
+    let mut error = None;
+    for (resource, amount) in delta.iter() {
         let current_resource = resources.entry(resource.clone()).or_insert(0);
         match amount {
             Amount::Gain(_) => {}
@@ -85,7 +168,7 @@ pub fn apply_job(mut resources: Resources, job: Job) -> Result<Resources, String
             Amount::Spend(delta) |
             Amount::Catalyst(delta) => {
                 if delta > current_resource {
-                    return Err(format!(""));
+                    error = Some(format!("Not enough {}", resource));
                 }
             }
             Amount::SpendX(delta) |
@@ -103,14 +186,14 @@ pub fn apply_job(mut resources: Resources, job: Job) -> Result<Resources, String
                     }
                 } else {
                     if delta < current_resource {
-                        return Err(format!("This button is configured incorrectly, with a zero quantity for resource {:?}.", current_resource))
+                        error = Some(format!("This button is configured incorrectly, with a zero quantity for resource {:?}.", current_resource));
                     }
                 }
             }
         }
     }
-    for (resource, amount) in job.delta.into_iter() {
-        let current_resource = resources.entry(resource).or_insert(0);
+    for (resource, amount) in delta.iter() {
+        let current_resource = resources.entry(resource.clone()).or_insert(0);
         match amount {
             Amount::Gain(delta) => {
                 *current_resource += delta;
@@ -125,9 +208,7 @@ pub fn apply_job(mut resources: Resources, job: Job) -> Result<Resources, String
             Amount::SpendX(delta_per) => {
                 *current_resource -= delta_per * x.unwrap_or(0);
             }
-            Amount::CatalystX(_delta_per) => {
-
-            }
+            Amount::CatalystX(_delta_per) => {}
         }
         // if delta.is_negative() {
         //     if *current_resource > delta as u64 {
@@ -139,36 +220,41 @@ pub fn apply_job(mut resources: Resources, job: Job) -> Result<Resources, String
         //     *current_resource += delta as u64;
         // }
     }
-    Ok(resources)
-}
 
-pub fn apply_jobs(mut resources: Resources, jobs: Vec<Job>) -> Result<Resources, String> {
-    for job in jobs {
-        resources = apply_job(resources, job)?;
+    if let Some(error) = error {
+        (resources, Err(error))
+    } else {
+        (resources, Ok(()))
     }
-    Ok(resources)
 }
 
-
-pub fn apply_jobs_partial(mut resources: Resources, jobs: Vec<Job>) -> Resources {
-    for job in jobs {
-        match apply_job(resources.clone(), job) {
-            Ok(new_resources) => {
-                resources = new_resources;
-            }
-            Err(_) => {}
-        }
-    }
-    resources
-}
-
-pub fn get_job_success_zip(resources: Resources, jobs: Vec<Job>) -> Vec<(Job, bool)> {
-    let mut results = Vec::new();
-    for index in 0..jobs.len() {
-        let previous_resources = apply_jobs_partial(resources.clone(), jobs[0..index].to_vec());
-        let job = jobs.get(index).expect("This index should be valid.").clone();
-        let success = apply_job(previous_resources, job.clone()).is_ok();
-        results.push((job, success));
-    }
-    results
-}
+// pub fn apply_jobs(mut resources: Resources, jobs: Vec<Job>) -> Result<Resources, String> {
+//     for job in jobs {
+//         resources = apply_job(resources, job)?;
+//     }
+//     Ok(resources)
+// }
+//
+//
+// pub fn apply_jobs_partial(mut resources: Resources, jobs: Vec<Job>) -> Resources {
+//     for job in jobs {
+//         match apply_job(resources.clone(), job) {
+//             Ok(new_resources) => {
+//                 resources = new_resources;
+//             }
+//             Err(_) => {}
+//         }
+//     }
+//     resources
+// }
+//
+// pub fn get_job_success_zip(resources: Resources, jobs: Vec<Job>) -> Vec<(Job, bool)> {
+//     let mut results = Vec::new();
+//     for index in 0..jobs.len() {
+//         let previous_resources = apply_jobs_partial(resources.clone(), jobs[0..index].to_vec());
+//         let job = jobs.get(index).expect("This index should be valid.").clone();
+//         let success = apply_job(previous_resources, job.clone()).is_ok();
+//         results.push((job, success));
+//     }
+//     results
+// }
